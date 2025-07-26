5571f5ee712ab3590f5934dc8d830e133804589d
EraldoMaia <eraldomaia.mec@outlook.com>
2025-07-26 17:23:08 -0300

    fix: Ajuste no tipo da coluna qtd_linhas

--- dataops/tb_log_exec/ddl_tb_log_exec.sql
+++ dataops/tb_log_exec/ddl_tb_log_exec.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS `data_quality.tb_log_exec` (
     projeto              STRING   OPTIONS(description = 'Projeto onde a execução ocorreu.'),
     dataset              STRING   OPTIONS(description = 'Dataset onde a execução ocorreu.'),
     table_name           STRING   OPTIONS(description = 'Tabela processada.'),
-    qtd_linhas           INT64    OPTIONS(description = 'Status da execução (SUCESSO ou ERRO).'),
+    qtd_linhas           INT64    OPTIONS(description = 'Quantidade de linhas inseridas na carga.'),
     dt_insercao_registro DATETIME OPTIONS(description = 'Data de inserção do registro na tabela.')
 )
 PARTITION BY DATE_TRUNC(dt_insercao_registro, MONTH)
